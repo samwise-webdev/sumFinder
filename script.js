41c7d5f04704768ff1c00388f52f4da6e4368f8c
Sam Burgett <samwise.burgett@outlook.com>
2023-11-02 18:50:30 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/* function range(start, end) {
+    let arrayRange = [];
+    for (let i = start; i <= end; i++) {
+        arrayRange.push[i];
+    }
+    console.log(arrayRange);
+}
+range(3,9); */
+
+/* function range(start, end) {
+    let arrayRange = [start, end];
+    for (let i = start; i <= end; i++) {
+        arrayRange.push[i];
+    }
+    console.log(arrayRange);
+}
+range(3,9); */
