59741c5631507fabad7eaf73eccaa8b5ecc5632a
Sam Burgett <samwise.burgett@outlook.com>
2023-11-02 18:54:09 -0400

    Breaking it down... first make an empty array

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ range(3,9); */
     console.log(arrayRange);
 }
 range(3,9); */
+
+// Breaking it down... first make an empty array
+function range(start, end) {
+    let arrayRange = [];
+    console.log(arrayRange);
+    }
+range(3,9);
+
