954a13ea9c966bfef70f650175955bb37ef22a01
Sam Burgett <samwise.burgett@outlook.com>
2023-11-02 19:09:03 -0400

    doesn't work; creates new arrays

--- script.js
+++ script.js
@@ -42,3 +42,14 @@ range(3,9) */
     console.log(arrayRange);
     }
 range(3,9) */
+
+//back to the first step;
+// this kind of works but it puts start and end at the beginning and creates a new array for each iteration instead of just adding the nubmers to the array.
+function range(start, end) {
+    let arrayRange = [start, end];
+    for (let i = start; i <= end; i++) {
+        arrayRange.push(i);
+        console.log(arrayRange);
+    }
+}
+range(3,9);
